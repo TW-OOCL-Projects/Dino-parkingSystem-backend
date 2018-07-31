2c6fde45fefc4ddbcec7738cdef12cf0e87e10ca
XIECI <cisnoc.xie@oocl.com>
2018-07-31 15:28:18 +0800

    add V1__Create_Tables.sql

---
+++ src/main/resources/db/migration/V1__Create_Tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE employee (
+    id  BIGINT   NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
+    name    VARCHAR(50)     NOT NULL,
+    email   VARCHAR(50)     NOT NULL,
+    phone_number    VARCHAR(20)     NOT NULL,
+    status  VARCHAR(20)     NOT NULL,
+    authority   VARCHAR(20)     NOT NULL
+);
+CREATE TABLE parking_lot (
+    id  BIGINT  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
+    name    VARCHAR(20)     NOT NULL,
+    `size`    INT     NOT NULL,
+    status  VARCHAR(20)     NOT NULL
+);
+CREATE TABLE `order` (
+    id  BIGINT  NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    type    VARCHAR(20)     NOT NULL,
+    parking_boy_id  BIGINT,
+    plate_number    VARCHAR(20)     NOT NULL,
+    status  VARCHAR(20)     NOT NULL
+);
+CREATE TABLE receipt (
+    id  BIGINT  NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    status  VARCHAR(20)     NOT NULL
+);
