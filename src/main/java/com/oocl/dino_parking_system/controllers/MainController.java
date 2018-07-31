083122fc852dc11a2340e86e606aa7ffb6049747
“zojian” <hellozojian@gmail.com>
2018-07-31 11:06:36 +0800

    test for heroku

--- src/main/java/com/oocl/dino_parking_system/controllers/MainController.java
+++ src/main/java/com/oocl/dino_parking_system/controllers/MainController.java
@@ -1,7 +1,13 @@
 package com.oocl.dino_parking_system.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @GetMapping(path = "/")
+    public String index(){
+        return "Hello,Dino!";
+    }
 }
+
--- src/main/java/com/oocl/dino_parking_system/entities/ExampleEntity.java
+++ src/main/java/com/oocl/dino_parking_system/entities/ExampleEntity.java
@@ -1,7 +1,4 @@
 package com.oocl.dino_parking_system.entities;
 
-import javax.persistence.Entity;
-
-@Entity
 public class ExampleEntity {
 }
