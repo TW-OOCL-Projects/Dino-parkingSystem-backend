8c6c4a7e63110d7010422ffc139495df3821d3df
oocl-vito <hellozojian@gmail.com>
2018-08-01 19:36:05 +0800

    [vito]exposeHeader Authorization

--- src/main/java/com/oocl/dino_parking_system/config/WebSecurityConfig.java
+++ src/main/java/com/oocl/dino_parking_system/config/WebSecurityConfig.java
@@ -62,7 +62,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.exposedHeaders("Authorization");
 			}
 		};
 	}
