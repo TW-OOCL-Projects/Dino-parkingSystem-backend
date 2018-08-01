1f53b3f17b99de95e3bcadddf54304c95c2168e8
oocl-vito <hellozojian@gmail.com>
2018-08-01 20:15:15 +0800

    [vito] generate some data before app start

--- src/main/java/com/oocl/dino_parking_system/init/GenerateData.java
+++ src/main/java/com/oocl/dino_parking_system/init/GenerateData.java
@@ -1,7 +1,9 @@
 package com.oocl.dino_parking_system.init;
 
+import com.oocl.dino_parking_system.entities.ParkingLot;
 import com.oocl.dino_parking_system.entities.Role;
 import com.oocl.dino_parking_system.entities.User;
+import com.oocl.dino_parking_system.repositorys.ParkingLotsRepository;
 import com.oocl.dino_parking_system.repositorys.RoleRepository;
 import com.oocl.dino_parking_system.repositorys.UserRepository;
 import com.oocl.dino_parking_system.services.UserService;
@@ -14,12 +16,15 @@ import java.util.Arrays;
 
 @Component
 @Order(value = 3)
-public class InitRoleAndUser implements CommandLineRunner {
+public class GenerateData implements CommandLineRunner {
     @Autowired
     private RoleRepository roleRepository;
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ParkingLotsRepository parkingLotsRepository;
 
     @Autowired
     private UserService userService;
@@ -42,5 +47,15 @@ public class InitRoleAndUser implements CommandLineRunner {
 	    user3.setRoles(Arrays.asList(manager));
         userRepository.saveAll(Arrays.asList(user1,user2,user3));
         System.out.println("===============用户初始化完成=============");
+
+	    ParkingLot parkingLot1 = new ParkingLot("oocl停车场1",20);
+	    ParkingLot parkingLot2 = new ParkingLot("oocl停车场2",10);
+	    ParkingLot parkingLot3 = new ParkingLot("oocl停车场3",20);
+	    ParkingLot parkingLot4 = new ParkingLot("oocl停车场4",25);
+	    ParkingLot parkingLot5 = new ParkingLot("oocl停车场5",20);
+	    ParkingLot parkingLot6 = new ParkingLot("oocl停车场6",14);
+	    ParkingLot parkingLot7 = new ParkingLot("oocl停车场7",20);
+	    parkingLotsRepository.saveAll(Arrays.asList(parkingLot1,parkingLot2,parkingLot3,
+			    parkingLot4,parkingLot5,parkingLot6,parkingLot7));
     }
 }
