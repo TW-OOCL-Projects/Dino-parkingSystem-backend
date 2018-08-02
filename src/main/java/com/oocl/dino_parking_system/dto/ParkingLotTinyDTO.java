e3267343ff024aa29c0acd84eea11da170be7ceb
“zojian” <hellozojian@gmail.com>
2018-08-03 00:06:56 +0800

    [vito]parkingLotTinyDTO add carNum feild

--- src/main/java/com/oocl/dino_parking_system/dto/ParkingLotTinyDTO.java
+++ src/main/java/com/oocl/dino_parking_system/dto/ParkingLotTinyDTO.java
@@ -9,12 +9,14 @@ public class ParkingLotTinyDTO {
 	private Long id;
 	private String name;
 	private int size;
+	private int carNum;
 	private String status;
 
 	public ParkingLotTinyDTO(ParkingLot parkingLot){
 		this.id = parkingLot.getId();
 		this.name = parkingLot.getName();
 		this.size = parkingLot.getSize();
+		this.carNum = parkingLot.getCarNum();
 		this.status = parkingLot.getStatus();
 	}
 
@@ -32,5 +34,9 @@ public class ParkingLotTinyDTO {
 
 	public String getStatus() {
 		return status;
+	}
+
+	public int getCarNum() {
+		return carNum;
 	}
 }
