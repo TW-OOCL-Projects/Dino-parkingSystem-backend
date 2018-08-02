575e94cc8e289e22ce3a783d5a7087bf0abf0178
“zojian” <hellozojian@gmail.com>
2018-08-03 00:24:49 +0800

    [vito]update generate date

--- src/main/java/com/oocl/dino_parking_system/init/GenerateData.java
+++ src/main/java/com/oocl/dino_parking_system/init/GenerateData.java
@@ -36,13 +36,20 @@ public class GenerateData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        /*
+         * 角色初始化数据
+         *
+         * */
         Role admin = new Role(1L,"ROLE_ADMIN");
         Role manager = new Role(2L,"ROLE_MANAGER");
         Role parkingBoy = new Role(3L,"ROLE_PARKINGBOY");
         Role user = new Role(4L,"ROLE_USER");
         roleRepository.saveAll(Arrays.asList(admin,manager,parkingBoy,user));
         System.out.println("===============角色初始化完成=============");
-
+        /*
+         * 员工初始化数据
+         *
+         * */
 
         User user1 = new User("admin","管理员A","1234","110@qq.com","13160675789");
         user1.setRoles(Arrays.asList(admin));
@@ -63,6 +70,10 @@ public class GenerateData implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(user1,user2,user3,user4,user5,user6,user7));
         System.out.println("===============用户初始化完成=============");
 
+        /*
+         * 停车场初始化数据
+         *
+         * */
 	    ParkingLot parkingLot1 = new ParkingLot("oocl停车场1",20);
 	    parkingLot1.setParkingBoy(user2);
 
@@ -87,19 +98,20 @@ public class GenerateData implements CommandLineRunner {
 	    parkingLot7.setCarNum(3);
 	    parkingLotsRepository.saveAll(Arrays.asList(parkingLot1,parkingLot2,parkingLot3,
 			    parkingLot4,parkingLot5,parkingLot6,parkingLot7));
-
+        System.out.println("===============停车场初始化完成=============");
 	    /*
 	    * 订单初始化数据
 	    *
 	    * */
 		LotOrder order1 = new LotOrder("粤DHC9767","1");
 		LotOrder order2 = new LotOrder("粤C76647","2");
-	    	LotOrder order3 = new LotOrder("粤C4767","3");
+		LotOrder order3 = new LotOrder("粤C4767","3");
 		LotOrder order4 = new LotOrder("粤CH73647","4");
-	    	LotOrder order5 = new LotOrder("粤AHC2767","5");
+		LotOrder order5 = new LotOrder("粤AHC2767","5");
 		LotOrder order6 = new LotOrder("粤VH71647","6");
-	    	LotOrder order7 = new LotOrder("粤BHC9467","7");
+		LotOrder order7 = new LotOrder("粤BHC9467","7");
 		LotOrder order8 = new LotOrder("粤NH76347","8");
 		orderRepository.saveAll(Arrays.asList(order1,order2,order3,order4,order5,order6,order7,order8));
+        System.out.println("===============未抢订单初始化完成=============");
     }
 }
