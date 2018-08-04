d7a4b389a460c343cae19298bc1cbf1866706150
Jerry <jerry.zhang@oocl.com>
2018-08-04 20:13:57 +0800

    [Jerry]complete search of parkingLot

--- src/main/java/com/oocl/dino_parking_system/controllers/ParkingLotController.java
+++ src/main/java/com/oocl/dino_parking_system/controllers/ParkingLotController.java
@@ -1,6 +1,7 @@
 package com.oocl.dino_parking_system.controllers;
 
 import com.oocl.dino_parking_system.dto.ParkingLotDashBoardDTO;
+import com.oocl.dino_parking_system.dto.ParkingLotTinyDTO;
 import com.oocl.dino_parking_system.entities.ParkingLot;
 import com.oocl.dino_parking_system.services.ParkingLotsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,92 +13,100 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/parkingLots")
 public class ParkingLotController {
-	private final ParkingLotsService parkingLotsService;
+    private final ParkingLotsService parkingLotsService;
 
-	@Autowired
-	public ParkingLotController(ParkingLotsService parkingLotsService) {
-		this.parkingLotsService = parkingLotsService;
-	}
+    @Autowired
+    public ParkingLotController(ParkingLotsService parkingLotsService) {
+        this.parkingLotsService = parkingLotsService;
+    }
 
-	@Transactional
-	@PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity createParkingLots(@RequestBody ParkingLot parkingLot) {
-		if (parkingLotsService.createParkingLots(parkingLot)) {
-			return ResponseEntity.status(HttpStatus.CREATED).build();
-		} else {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		}
-	}
+    @Transactional
+    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity createParkingLots(@RequestBody ParkingLot parkingLot) {
+        if (parkingLotsService.createParkingLots(parkingLot)) {
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 
-	@PreAuthorize("hasRole('ADMIN')")
-	@Transactional
-	@GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity getAllParkingLots(@RequestParam(value = "id", required = false) Long id,
-	                                        @RequestParam(value = "name", required = false) String name,
-	                                        @RequestParam(value = "eq", required = false) Integer eq,
-	                                        @RequestParam(value = "gt", required = false) Integer gt,
-	                                        @RequestParam(value = "lt", required = false) Integer lt) {
-		if (id != null) {
-			// 根据ID查找
-			return new ResponseEntity<>(parkingLotsService.findParkingLotById(id), HttpStatus.OK);
-		}
-		if (name != null && eq != null) {
-			// TODO:组合查询
-		} else if (name != null) {
-			// TODO:根据名字模糊查询
-//			return new ResponseEntity<>(parkingLotsService.findAllParkingLotsByName(name), HttpStatus.OK);
-		} else if (gt != null && lt != null) {
-			// 根据大小范围查找
-			return new ResponseEntity<>(parkingLotsService.findAllParkingLotsBySizeBetween(lt, gt), HttpStatus.OK);
-		} else if (gt != null) {
-			// 大于等于
-			return new ResponseEntity<>(parkingLotsService.findAllParkingLotsBySizeGreaterThanEqual(gt), HttpStatus.OK);
-		} else if (lt != null) {
-			// TODO: 小于等于
-//			return new ResponseEntity<>(parkingLotsService.findAllParkingLotsBySizeLessThanEqual(lt), HttpStatus.OK);
-		}
-		return new ResponseEntity<>(parkingLotsService.getAllParkingLots(), HttpStatus.OK);
+    //    @PreAuthorize("hasRole('ADMIN')")
+    @Transactional
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getAllParkingLots(@RequestParam(value = "id", required = false) Long id,
+                                            @RequestParam(value = "name", required = false) String name,
+                                            @RequestParam(value = "eq", required = false) Integer eq,
+                                            @RequestParam(value = "gt", required = false) Integer gt,
+                                            @RequestParam(value = "lt", required = false) Integer lt) {
+        List<ParkingLotTinyDTO> parkingLotTinyDTOS = new ArrayList<>();
 
-	}
+        if (id != null) {
+            // 根据ID查找
+            return new ResponseEntity<>(parkingLotsService.findParkingLotById(id), HttpStatus.OK);
+        }
+        // TODO:组合查询
+        if (name != null && eq != null) {
+            parkingLotTinyDTOS = parkingLotsService.findAllParkingLotsByNameAndSize(name, eq);
+        } else if (name != null) {
+//            System.out.println(name);
+            // TODO:根据名字查询
+            parkingLotTinyDTOS = parkingLotsService.findAllParkingLotsByName(name);
+        } else if (gt != null && lt != null) {
+            // 根据大小范围查找
+            parkingLotTinyDTOS = parkingLotsService.findAllParkingLotsBySizeBetween(gt, lt);
+        } else if (gt != null) {
+            // 大于等于
+            parkingLotTinyDTOS = parkingLotsService.findAllParkingLotsBySizeGreaterThanEqual(gt);
+        } else if (lt != null) {
+            // TODO: 小于等于
+            parkingLotTinyDTOS = parkingLotsService.findAllParkingLotsBySizeLessThanEqual(lt);
+        } else {
+            parkingLotTinyDTOS = parkingLotsService.getAllParkingLots();
+        }
 
-	@Transactional
-	@PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity updateParkingLots(@PathVariable long id, @RequestBody ParkingLot parkingLot) {
-		if (parkingLotsService.updateParkingLots(id, parkingLot)) {
-			return ResponseEntity.status(HttpStatus.OK).build();
-		} else {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		}
-	}
+        return new ResponseEntity<>(parkingLotTinyDTOS, HttpStatus.OK);
 
-	@Transactional
-	@PatchMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity changeParkingLotStatus(@PathVariable long id) {
-		if (parkingLotsService.changeParkingLotStatus(id)) {
-			return ResponseEntity.status(HttpStatus.OK).build();
-		} else {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		}
-	}
+    }
 
-	@PreAuthorize("hasRole('ADMIN')")
-	@Transactional
-	@GetMapping(path = "/dashboard")
-	public List<ParkingLotDashBoardDTO> findAllParkingLotDashBoard() {
-		return parkingLotsService.findAllParkingLotDashBoard();
-	}
+    @Transactional
+    @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity updateParkingLots(@PathVariable long id, @RequestBody ParkingLot parkingLot) {
+        if (parkingLotsService.updateParkingLots(id, parkingLot)) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 
-	@PreAuthorize("hasRole('ADMIN')")
-	@Transactional
-	@GetMapping(path = "/dashboard/page/{page}/pageSize/{size}")
-	public List<ParkingLotDashBoardDTO> findAllParkingLotDashBoard(@PathVariable int page,
-	                                                               @PathVariable int size) {
-		return parkingLotsService.findAllParkingLotDashBoardByPaging(new PageRequest(page, size));
-	}
+    @Transactional
+    @PatchMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity changeParkingLotStatus(@PathVariable long id) {
+        if (parkingLotsService.changeParkingLotStatus(id)) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Transactional
+    @GetMapping(path = "/dashboard")
+    public List<ParkingLotDashBoardDTO> findAllParkingLotDashBoard() {
+        return parkingLotsService.findAllParkingLotDashBoard();
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Transactional
+    @GetMapping(path = "/dashboard/page/{page}/pageSize/{size}")
+    public List<ParkingLotDashBoardDTO> findAllParkingLotDashBoard(@PathVariable int page,
+                                                                   @PathVariable int size) {
+        return parkingLotsService.findAllParkingLotDashBoardByPaging(new PageRequest(page, size));
+    }
 
 }
