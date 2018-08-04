02f4d43faa1898ff715c10fd7e9d5ea1d70a92de
Jerry <jerry.zhang@oocl.com>
2018-08-04 13:40:29 +0800

    [Jerry]add user successfully

--- src/main/java/com/oocl/dino_parking_system/config/WebSecurityConfig.java
+++ src/main/java/com/oocl/dino_parking_system/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,6 +26,7 @@ import java.util.Arrays;
  * Created by Vito Zhuang on 7/31/2018.
  */
 @EnableWebSecurity
+@EnableGlobalMethodSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
@@ -48,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().cors().and().authorizeRequests()
-				.antMatchers("/**").permitAll()
+//				.antMatchers("/**").permitAll()
 //				.antMatchers("/manager/**").hasRole("MANAGER")
 //				.antMatchers("/admin/**").hasRole("ADMIN")
 //				.antMatchers("/parkingboy/**").hasRole("PARKINGBOY")
--- src/test/java/com/oocl/dino_parking_system/repositorys/UserRepositoryTest.java
+++ src/test/java/com/oocl/dino_parking_system/repositorys/UserRepositoryTest.java
@@ -2,10 +2,12 @@ package com.oocl.dino_parking_system.repositorys;
 
 import com.oocl.dino_parking_system.entities.LotOrder;
 import com.oocl.dino_parking_system.entities.User;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
@@ -24,11 +26,19 @@ public class UserRepositoryTest {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private TestEntityManager manager;
+
+    @After
+    public void tearDown()throws Exception{
+        manager.clear();
+    }
+
     @Test
     public void demotest() {
         User user = new User("a", "111@as.com", "1324564", "aas", "adf");
         LotOrder lotOrder = new LotOrder("asd", "asdf", "adf", "1234");
-        lotOrder.setUser(user);
+        lotOrder.setParkingBoy(user);
         orderRepository.save(lotOrder);
         LotOrder o = orderRepository.findById(1L).get();
         user.setLotOrders(Arrays.asList(o));
@@ -40,5 +50,15 @@ public class UserRepositoryTest {
         assertThat(lotOrders.get(0).getType(), is("asd"));
         assertThat(users.get(0).getLotOrders().get(0).getType(), is("asd"));
 
+    }
+
+    @Test
+    public void should_add_user_successfully(){
+        //given
+        User user = new User("a", "111@as.com", "1324564", "aas", "adf");
+        //when
+        employeeRepository.save(user);
+        //then
+        assertThat(employeeRepository.findAll().size(),is(1));
     }
 }
